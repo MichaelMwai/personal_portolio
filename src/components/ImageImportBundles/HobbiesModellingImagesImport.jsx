aa83cf9a6aac6f64873366a855d3f9dca282f69e
Michael Mwai <michaeldmwai@gmail.com>
2024-11-05 21:50:24 +0300

    Modelling added

---
+++ src/components/ImageImportBundles/HobbiesModellingImagesImport.jsx
@@ -0,0 +1,5 @@
+import modelling_1 from "../../assets/hobbies/modelling/modelling_1.jpg";
+import modelling_2 from "../../assets/hobbies/modelling/modelling_2.jpg";
+import modelling_3 from "../../assets/hobbies/modelling/modelling_3.jpg";
+
+export const images = [modelling_1, modelling_2, modelling_3];
---
+++ src/components/Projects/ProjectsData.jsx
@@ -0,0 +1,23 @@
+import RecipeFinderGIF from "../../assets/works/comingSoon.gif";
+
+// import EtchASketchGIF from "../../assets/works/etchsketch.gif";
+// finish up and test upload
+
+export const projectsData = [
+  {
+    image: RecipeFinderGIF,
+    title: "My First Upload",
+    description:
+      "Maybe a full-stack TypeScript application leveraging React with custom hooks, context for state management, and styled-components for dynamic theming and responsive design.",
+    link: "https://github.com/MichaelMwai",
+    demo: "#",
+  },
+  {
+    image: RecipeFinderGIF,
+    title: "My Second Upload",
+    description:
+      "Perhaps an interactive web project using JavaScript, HTML, and CSS. It employs object-oriented programming for game logic, features dynamic DOM manipulation.",
+    link: "https://github.com/MichaelMwai",
+    demo: "#",
+  },
+];

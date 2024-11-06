73746ac8a6d942f8ca09ae636d963a7934e3395e
Michael Mwai <michaeldmwai@gmail.com>
2024-11-06 18:47:43 +0300

    Update on my fitness info'

---
+++ src/components/ImageImportBundles/HobbiesFitnessImagesImport.jsx
@@ -0,0 +1,7 @@
+import fitness_1 from "../../assets/hobbies/fitness/fitness_1.jpg";
+import fitness_2 from "../../assets/hobbies/fitness/fitness_2.jpg";
+import fitness_4 from "../../assets/hobbies/fitness/fitness_4.jpg";
+import fitness_3 from "../../assets/hobbies/fitness/fitness_3.jpg";
+import fitness_5 from "../../assets/hobbies/fitness/fitness_5.jpg";
+
+export const images = [fitness_1, fitness_2, fitness_3, fitness_4, fitness_5];
